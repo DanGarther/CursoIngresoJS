f1bd9d5dc39faeb97821e355b5cd306e6e741be5
DanGarther <47385923+DanGarther@users.noreply.github.com>
2019-07-10 12:20:56 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
+
+	nota = Math.floor((Math.random() * 10) + 1);
+
+	if(nota == 9 || nota == 10){
+		alert("EXCELENTE (" + nota + ")");
+	}
+
+	else if(nota > 4){
+		alert("APROBÓ (" + nota + ")");
+	}
+
+	else{
+		alert("Vamos, la proxima se puede (" + nota + ")")
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,26 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
 
+if(edad >= 18){
+
+    alert("Usted es un adulto.");
+
+}
+
+else if(edad >= 13 && edad <= 17){
+
+    alert("Usted es un adolecente.");
+}
+
+else{
+
+    alert("Usted es un niño.");
+
+}
 
 
 }//FIN DE LA FUNCIÓN
